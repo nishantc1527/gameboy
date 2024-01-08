605cad43d0125af9332f13dbb56f2b765621dc5c
Nishant Chatterjee <52992695+nishantc1527@users.noreply.github.com>
2024-01-07 19:54:59 -0800

    Update func.h

--- func.h
+++ func.h
@@ -74,34 +74,38 @@ void st_c_sub(byte var1, byte var2) {
 }
 
 byte r_mem(dbyte loc) {
-  if(mem[0xFF50] || loc >= 0x100) return mem[loc];
+  if (mem[0xFF50] || loc >= 0x100) return mem[loc];
   else return brom[loc];
 }
 
 void w_mem(dbyte loc, byte val) {
-  if(loc == 0xFF46) dma = 1;
-  if (loc == 0xFF04) val = 0;
-  mem[loc] = val;
+    if (loc == 0xFF46) dma = 1;
+    if (loc == 0xFF04) val = 0;
+    mem[loc] = val;
 }
 
 byte rd8() {
-  if(r_mem(0xFF50)) return mem[++ PC];
-  else return brom[++ PC];
+    if (r_mem(0xFF50)) return mem[++PC];
+    else return brom[++PC];
 }
 
 dbyte rd16() {
-  dbyte addr1 = ++ PC;
-  dbyte addr2 = ++ PC;
-  if(r_mem(0xFF50)) return ((dbyte) mem[addr2] << 8) | (dbyte) mem[addr1];
-  else return ((dbyte) brom[addr2] << 8) | (dbyte) brom[addr1];
+    dbyte addr1 = ++PC;
+    dbyte addr2 = ++PC;
+    if (r_mem(0xFF50)) return ((dbyte)mem[addr2] << 8) | (dbyte)mem[addr1];
+    else return ((dbyte)brom[addr2] << 8) | (dbyte)brom[addr1];
 }
 
 void psh16(dbyte val) {
-  byte val1 = (byte) (val >> 8);
-  byte val2 = (byte) val;
-  w_mem(SP - 1, val1);
-  w_mem(SP - 2, val2);
-  SP -= 2;
+    if (val == 0xB702) {
+        printf("HERE\n");
+        SDL_Delay(5000);
+    }
+    byte val1 = (byte)(val >> 8);
+    byte val2 = (byte)val;
+    w_mem(SP - 1, val1);
+    w_mem(SP - 2, val2);
+    SP -= 2;
 }
 
 dbyte pop16() {
@@ -151,6 +155,10 @@ void kp() {
   PC --;
 }
 
+int c_adc(byte reg) {
+    return c_add(reg + gt_flg(FLG_C));
+}
+
 int c_add(byte reg) {
   st_h_add(A, reg);
   st_c_add(A, reg);
@@ -177,6 +185,27 @@ int c_bit(byte reg, int bit) {
   return 8;
 }
 
+int c_call(int flg) {
+    if (flg) {
+        psh16(PC + 3);
+        PC = rd16();
+        kp();
+        return 24;
+    }
+    else {
+        rd16();
+        return 12;
+    }
+}
+
+int c_cp(byte reg) {
+    st_z(A - reg);
+    st_flg(FLG_N);
+    st_h_sub(A, reg);
+    st_c_sub(A, reg);
+    return 4;
+}
+
 int c_cpl(byte* reg) {
   *reg = ~*reg;
   st_flg(FLG_N);
@@ -200,6 +229,27 @@ int c_inc(byte* reg) {
   return 4;
 }
 
+int c_jp8(int flg) {
+    if (flg) {
+        PC += (signed char)rd8();
+        return 12;
+    }
+    else {
+        rd8();
+        return 8;
+    }
+}
+
+int c_jp16(int flg) {
+    if (flg) {
+        PC = rd16();
+        kp();
+        return 16;
+    }
+    else rd16();
+    return 12;
+}
+
 int c_or(byte reg) {
   A |= reg;
   st_z(A);
@@ -214,6 +264,17 @@ int c_res(byte* reg, int bit) {
   return 8;
 }
 
+int c_ret(int flg) {
+    if (flg) {
+        PC = pop16();
+        kp();
+        return 16;
+    }
+    else {
+        return 8;
+    }
+}
+
 int c_rr(byte* reg) {
   byte carry = gt_flg(FLG_C);
   if(*reg & 1) st_flg(FLG_C);
@@ -226,9 +287,20 @@ int c_rr(byte* reg) {
   return 8;
 }
 
+int c_rrc(byte* reg) {
+    int carry = *reg & 1;
+    if (carry) st_flg(FLG_C);
+    else cl_flg(FLG_C);
+    *reg = (*reg >> 1) | (carry << 7);
+    cl_flg(FLG_Z);
+    cl_flg(FLG_N);
+    cl_flg(FLG_H);
+    return 4;
+}
+
 int c_rl(byte* reg) {
   int carry = gt_flg(FLG_C);
-  st_c_rl(C);
+  st_c_rl(*reg);
   *reg = (*reg << 1) | carry;
   st_z(*reg);
   cl_flg(FLG_N);
@@ -236,11 +308,31 @@ int c_rl(byte* reg) {
   return 8;
 }
 
+int c_rlc(byte* reg) {
+    int carry = gt_bt(*reg, 7);
+    st_c_rl(*reg);
+    *reg = (*reg << 1) | carry;
+    st_z(*reg);
+    cl_flg(FLG_N);
+    cl_flg(FLG_H);
+    return 8;
+}
+
 int c_rst(byte loc) {
   psh16(PC + 1);
   PC = loc;
   kp();
   return 16;
+}
+
+int c_sbc(byte reg) {
+    byte sub = reg + gt_flg(FLG_C);
+    st_h_sub(A, sub);
+    st_c_sub(A, sub);
+    A -= sub;
+    st_z(A);
+    st_flg(FLG_N);
+    return 4;
 }
 
 int c_srl(byte* reg) {
@@ -256,6 +348,24 @@ int c_srl(byte* reg) {
 int c_set(byte* reg, int bit) {
   st_bt(reg, bit);
   return 16;
+}
+
+int c_sla(byte* reg) {
+    st_c_rl(*reg);
+    *reg = *reg << 1;
+    st_z(*reg);
+    cl_flg(FLG_N);
+    cl_flg(FLG_H);
+    return 8;
+}
+
+int c_sub(byte reg) {
+    st_h_sub(A, reg);
+    st_c_sub(A, reg);
+    A -= reg;
+    st_z(A);
+    st_flg(FLG_N);
+    return 4;
 }
 
 int c_swp(byte* reg) {
@@ -296,7 +406,7 @@ void do_intr(int intr) {
             printf("SERIAL INTERRUPT\n");
             break;
         case 4:
-            printf("JOYPAD INTERRUPT");
+            printf("JOYPAD INTERRUPT\n");
             break;
         }
         cl_bt(&mem[0xFF0F], intr);
@@ -318,8 +428,7 @@ int chck_intr() {
     return 0;
 }
 
-void chck_in() {
-    memset(in, 0, sizeof(in));
+int chck_in() {
     while (SDL_PollEvent(&evt)) {
         if (evt.type == SDL_QUIT) return 1;
         else if (evt.type == SDL_KEYDOWN) {
@@ -331,10 +440,10 @@ void chck_in() {
                 in[BTN_B] = 1;
                 break;
             case SDLK_RETURN:
-                in[BTN_STRT] = 1;
+                in[BTN_START] = 1;
                 break;
             case SDLK_LSHIFT: case SDLK_RSHIFT:
-                in[BTN_SLCT] = 1;
+                in[BTN_SELECT] = 1;
                 break;
             case SDLK_UP:
                 in[BTN_UP] = 1;
@@ -350,6 +459,34 @@ void chck_in() {
                 break;
             }
         }
+        else if (evt.type == SDL_KEYUP) {
+            switch (evt.key.keysym.sym) {
+            case SDLK_s:
+                in[BTN_A] = 0;
+                break;
+            case SDLK_a:
+                in[BTN_B] = 0;
+                break;
+            case SDLK_RETURN:
+                in[BTN_START] = 0;
+                break;
+            case SDLK_LSHIFT: case SDLK_RSHIFT:
+                in[BTN_SELECT] = 0;
+                break;
+            case SDLK_UP:
+                in[BTN_UP] = 0;
+                break;
+            case SDLK_DOWN:
+                in[BTN_DOWN] = 0;
+                break;
+            case SDLK_LEFT:
+                in[BTN_LEFT] = 0;
+                break;
+            case SDLK_RIGHT:
+                in[BTN_RIGHT] = 0;
+                break;
+            }
+        }
     }
     byte in_reg = JOYP;
     in_reg |= 0xF;
@@ -362,21 +499,24 @@ void chck_in() {
     if (!gt_bt(in_reg, 5)) {
         if (in[BTN_A]) cl_bt(&in_reg, 0);
         if (in[BTN_B]) cl_bt(&in_reg, 1);
-        if (in[BTN_SLCT]) cl_bt(&in_reg, 2);
-        if (in[BTN_STRT]) cl_bt(&in_reg, 3);
+        if (in[BTN_SELECT]) cl_bt(&in_reg, 2);
+        if (in[BTN_START]) cl_bt(&in_reg, 3);
     }
-    if (in_reg != JOYP) req_intr(4);
+    int intr = 0;
+    for (int i = 0; i < 8; i++) if ((JOYP >> i) & 1 && !((in_reg >> i) & 1)) intr = 1;
+    if (intr) req_intr(4);
     w_mem(0xFF00, in_reg);
+    return 0;
 }
 
 void chck_dma() {
-  if(dma) {
-    dma = 0;
-    byte src = DMA * 0x100;
-    for(int t = 0; t < 0xA0; t ++) {
-      w_mem(0xFE00 + t, r_mem(src + t));
+    if (dma) {
+        dma = 0;
+        byte src = DMA * 0x100;
+        for (int t = 0; t < 0xA0; t++) {
+            w_mem(0xFE00 + t, r_mem(src + t));
+        }
     }
-  }
 }
 
 void upd_tim() {
@@ -384,7 +524,6 @@ void upd_tim() {
     byte div = DIV;
     if (STOP) div = 0;
     w_mem(0xFF04, div);
-
     byte tima = TIMA;
     if (tima == 0xFF) {
         tima = TMA;
@@ -395,7 +534,7 @@ void upd_tim() {
 }
 
 void dbg() {
-  printf("{\n\tAF: $%04X\n\tBC: $%04X\n\tDE: $%04X\n\tHL: $%04X\n\tSP: $%04X\n\tPC: $%04X\n\tZERO FLAG: %d\n\tSUBTRACTION FLAG: %d\n\tHALF CARRY FLAG: %d\n\tCARRY FLAG: %d\n}\n", gt_AF(), gt_BC(), gt_DE(), gt_HL(), SP, PC, gt_flg(FLG_Z), gt_flg(FLG_N), gt_flg(FLG_H), gt_flg(FLG_C));
+    printf("{\n\tAF: $%04X\n\tBC: $%04X\n\tDE: $%04X\n\tHL: $%04X\n\tSP: $%04X\n\tPC: $%04X\n\tZERO FLAG: %d\n\tSUBTRACTION FLAG: %d\n\tHALF CARRY FLAG: %d\n\tCARRY FLAG: %d\n}\n", gt_AF(), gt_BC(), gt_DE(), gt_HL(), SP, PC, gt_flg(FLG_Z), gt_flg(FLG_N), gt_flg(FLG_H), gt_flg(FLG_C));
 }
 
 #endif
