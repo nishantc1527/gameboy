8b1aad6df8c34c3514d49ada32b452bdc45246f6
Nishant Chatterjee <52992695+nishantc1527@users.noreply.github.com>
2024-01-07 19:55:12 -0800

    Update gbemu.c

--- gbemu.c
+++ gbemu.c
@@ -11,14 +11,24 @@ int update() {
     scn = 456;
     frame = 0;
     dma = 0;
+    int d = 0;
 
     while (!frame) {
+        d = 0;
+        switch (PC) { // PC
+        case 0xB702:
+            d = 1;
+        }
         if (HALT) {
             if (chck_intr()) HALT = 0;
             scn -= 4;
         }
         else {
-            int curr = exec(r_mem(PC));
+            int curr = exec();
+            if (d) {
+                dbg();
+                SDL_Delay(5000);
+            }
             if (curr == -1) return 1;
             OP_CNT++;
             PC++;
@@ -40,12 +50,12 @@ int update() {
 int main(int argc, char* argv[]) {
     init_reg();
     init_dsp();
-    FILE* b_rom;
+    FILE* boot_rom;
     FILE* rom;
-    fopen_s(&b_rom, "bootrom.rom", "rb");
+    fopen_s(&boot_rom, "bootrom.rom", "rb");
     fopen_s(&rom, "space.gb", "rb");
     fread(mem, 0x8000, 1, rom);
-    fread(brom, 0x100, 1, b_rom);
+    fread(brom, 0x100, 1, boot_rom);
     while (1) {
         if (update()) break;
         if (rndr()) break;
