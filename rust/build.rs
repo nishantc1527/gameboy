3b4a4d053c1cd182d3b6dc0b7528a3a85f8e2cf1
Nishant Chatterjee <nishantc1527@gmail.com>
2026-02-14 03:16:09 -0800

    pkg-config for sdl3

--- rust/build.rs
+++ rust/build.rs
@@ -2,12 +2,20 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let sdl3 = pkg_config::Config::new()
+        .atleast_version("3.0")
+        .probe("sdl3")
+        .expect("Couldn't find SDL3 via pkg-config");
     let bindings = bindgen::Builder::default()
         .header("sdl_wrapper.h")
+        .clang_args(
+            sdl3.include_paths
+                .iter()
+                .map(|path| format!("-I{}", path.to_string_lossy())),
+        )
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Couldn't generate bindings");
-
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("sdl_bindings.rs"))
