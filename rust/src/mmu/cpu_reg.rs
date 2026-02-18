c0d5fc54c6451c91aa0e8e741dfc1906d4f35481
Nishant Chatterjee <nishantc1527@gmail.com>
2026-02-18 11:34:11 -0800

    Verified

--- rust/src/mmu/cpu_reg.rs
+++ rust/src/mmu/cpu_reg.rs
@@ -1,9 +1,18 @@
-pub const JOYP: u16 = 0xFF00;
-pub const SB: u16 = 0xFF01;
-pub const SC: u16 = 0xFF02;
-pub const DIV: u16 = 0xFF04;
-pub const TIMA: u16 = 0xFF05;
-pub const TMA: u16 = 0xFF06;
-pub const TAC: u16 = 0xFF07;
-pub const IF: u16 = 0xFF0F;
-pub const IE: u16 = 0xFFFF;
+#[unsafe(no_mangle)]
+pub static JOYP: u16 = 0xFF00;
+#[unsafe(no_mangle)]
+pub static SB: u16 = 0xFF01;
+#[unsafe(no_mangle)]
+pub static SC: u16 = 0xFF02;
+#[unsafe(no_mangle)]
+pub static DIV: u16 = 0xFF04;
+#[unsafe(no_mangle)]
+pub static TIMA: u16 = 0xFF05;
+#[unsafe(no_mangle)]
+pub static TMA: u16 = 0xFF06;
+#[unsafe(no_mangle)]
+pub static TAC: u16 = 0xFF07;
+#[unsafe(no_mangle)]
+pub static IF: u16 = 0xFF0F;
+#[unsafe(no_mangle)]
+pub static IE: u16 = 0xFFFF;
--- rust/src/mmu/ppu_reg.rs
+++ rust/src/mmu/ppu_reg.rs
@@ -1,12 +1,24 @@
-pub const LCDC: u16 = 0xFF40;
-pub const LCD_STAT: u16 = 0xFF41;
-pub const SCY: u16 = 0xFF42;
-pub const SCX: u16 = 0xFF43;
-pub const LY: u16 = 0xFF44;
-pub const LYC: u16 = 0xFF45;
-pub const DMA: u16 = 0xFF46;
-pub const BGP: u16 = 0xFF47;
-pub const OBP0: u16 = 0xFF48;
-pub const OBP1: u16 = 0xFF49;
-pub const WY: u16 = 0xFF4A;
-pub const WX: u16 = 0xFF4B;
+#[unsafe(no_mangle)]
+pub static LCDC: u16 = 0xFF40;
+#[unsafe(no_mangle)]
+pub static LCD_STAT: u16 = 0xFF41;
+#[unsafe(no_mangle)]
+pub static SCY: u16 = 0xFF42;
+#[unsafe(no_mangle)]
+pub static SCX: u16 = 0xFF43;
+#[unsafe(no_mangle)]
+pub static LY: u16 = 0xFF44;
+#[unsafe(no_mangle)]
+pub static LYC: u16 = 0xFF45;
+#[unsafe(no_mangle)]
+pub static DMA: u16 = 0xFF46;
+#[unsafe(no_mangle)]
+pub static BGP: u16 = 0xFF47;
+#[unsafe(no_mangle)]
+pub static OBP0: u16 = 0xFF48;
+#[unsafe(no_mangle)]
+pub static OBP1: u16 = 0xFF49;
+#[unsafe(no_mangle)]
+pub static WY: u16 = 0xFF4A;
+#[unsafe(no_mangle)]
+pub static WX: u16 = 0xFF4B;
