56365e9467f42aa73a19ae8aae190af5e135c05c
Nishant Chatterjee <nishantc1527@gmail.com>
2026-03-17 23:14:21 -0700

    fix gitignore

--- rust/src/mmu/cpu_reg.rs
+++ rust/src/mmu/cpu_reg.rs
@@ -1,18 +1,18 @@
-#[unsafe(no_mangle)]
-pub static JOYP: u16 = 0xFF00;
-#[unsafe(no_mangle)]
-pub static SB: u16 = 0xFF01;
-#[unsafe(no_mangle)]
-pub static SC: u16 = 0xFF02;
-#[unsafe(no_mangle)]
-pub static DIV: u16 = 0xFF04;
-#[unsafe(no_mangle)]
-pub static TIMA: u16 = 0xFF05;
-#[unsafe(no_mangle)]
-pub static TMA: u16 = 0xFF06;
-#[unsafe(no_mangle)]
-pub static TAC: u16 = 0xFF07;
-#[unsafe(no_mangle)]
-pub static IF: u16 = 0xFF0F;
-#[unsafe(no_mangle)]
-pub static IE: u16 = 0xFFFF;
+#[allow(dead_code)]
+pub const JOYP: u16 = 0xFF00;
+#[allow(dead_code)]
+pub const SB: u16 = 0xFF01;
+#[allow(dead_code)]
+pub const SC: u16 = 0xFF02;
+#[allow(dead_code)]
+pub const DIV: u16 = 0xFF04;
+#[allow(dead_code)]
+pub const TIMA: u16 = 0xFF05;
+#[allow(dead_code)]
+pub const TMA: u16 = 0xFF06;
+#[allow(dead_code)]
+pub const TAC: u16 = 0xFF07;
+#[allow(dead_code)]
+pub const IF: u16 = 0xFF0F;
+#[allow(dead_code)]
+pub const IE: u16 = 0xFFFF;
--- rust/src/mmu/ppu_reg.rs
+++ rust/src/mmu/ppu_reg.rs
@@ -1,24 +1,24 @@
-#[unsafe(no_mangle)]
-pub static LCDC: u16 = 0xFF40;
-#[unsafe(no_mangle)]
-pub static LCD_STAT: u16 = 0xFF41;
-#[unsafe(no_mangle)]
-pub static SCY: u16 = 0xFF42;
-#[unsafe(no_mangle)]
-pub static SCX: u16 = 0xFF43;
-#[unsafe(no_mangle)]
-pub static LY: u16 = 0xFF44;
-#[unsafe(no_mangle)]
-pub static LYC: u16 = 0xFF45;
-#[unsafe(no_mangle)]
-pub static DMA: u16 = 0xFF46;
-#[unsafe(no_mangle)]
-pub static BGP: u16 = 0xFF47;
-#[unsafe(no_mangle)]
-pub static OBP0: u16 = 0xFF48;
-#[unsafe(no_mangle)]
-pub static OBP1: u16 = 0xFF49;
-#[unsafe(no_mangle)]
-pub static WY: u16 = 0xFF4A;
-#[unsafe(no_mangle)]
-pub static WX: u16 = 0xFF4B;
+#[allow(dead_code)]
+pub const LCDC: u16 = 0xFF40;
+#[allow(dead_code)]
+pub const LCD_STAT: u16 = 0xFF41;
+#[allow(dead_code)]
+pub const SCY: u16 = 0xFF42;
+#[allow(dead_code)]
+pub const SCX: u16 = 0xFF43;
+#[allow(dead_code)]
+pub const LY: u16 = 0xFF44;
+#[allow(dead_code)]
+pub const LYC: u16 = 0xFF45;
+#[allow(dead_code)]
+pub const DMA: u16 = 0xFF46;
+#[allow(dead_code)]
+pub const BGP: u16 = 0xFF47;
+#[allow(dead_code)]
+pub const OBP0: u16 = 0xFF48;
+#[allow(dead_code)]
+pub const OBP1: u16 = 0xFF49;
+#[allow(dead_code)]
+pub const WY: u16 = 0xFF4A;
+#[allow(dead_code)]
+pub const WX: u16 = 0xFF4B;
